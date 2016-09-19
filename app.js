9bdf550f56b4869f55fbf76d2cf8c3fd543d069e
ananya goel <ananyagoel@ananyas-MacBook-Air.local>
2016-09-20 00:26:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var knex=require('knex')({
+        client: 'pg',
+        connection: {
+            host     : '127.0.0.1',
+            user     : 'ananyagoel',
+            password : 'pizza',
+            database : 'blog_bookshelf',
+            // database: 'nodedb1',
+            charset  : 'utf8'
+        },
+        pool: {
+          min: 2,
+          max: 10
+        },
+        migrations: {
+          tableName: 'knex_migrations'
+        },
+        // debug: true
+});
+
+
+
+
+// User model
+var User = Bookshelf.Model.extend({
+    tableName: 'users'
+});
+// Post model
+var Post = Bookshelf.Model.extend({
+    tableName: 'posts',
+    hasTimestamps: true,
+    category: function () {
+      return this.belongsTo(Category, 'category_id');
+    },
+    tags: function () {
+        return this.belongsToMany(Tag);
+    },
+    author: function () {
+        return this.belongsTo(User);
+    }
+});
+// Category model
+var Category = Bookshelf.Model.extend({
+    tableName: 'categories',
+    posts: function () {
+       return this.hasMany(Post, 'category_id');
+    }
+});
+// Tag model
+var Tag = Bookshelf.Model.extend({
+    tableName: 'tags',
+    posts: function () {
+       return this.belongsToMany(Post);
+    }
+});
