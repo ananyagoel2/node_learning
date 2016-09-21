e2ad1d954bb44a9db7e5f6234b6f985b7a5e8a0b
ananya goel <ananyagoel@ananyas-MacBook-Air.local>
2016-09-21 12:28:38 +0530

    change mobile to string

---
+++ try.js
@@ -0,0 +1,14 @@
+var app = require("express")();
+
+app.get("/", function(httpRequest, httpResponse, next){
+    httpResponse.write("Hello");
+    //next(); //remove this and see what happens
+
+});
+
+app.get("/", function(httpRequest, httpResponse, next){
+    httpResponse.write(" World !!!");
+    httpResponse.end();
+});
+
+app.listen(8080);
